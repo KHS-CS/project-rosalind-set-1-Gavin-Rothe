7abb7b1efaf2213c0ecc873134e8527b4eb421b4
Gavin-Rothe <gavin.rothe@kentshill.org>
2026-01-21 09:14:29 -0500

    Update ProblemRNA.java

--- RNA/ProblemRNA.java
+++ RNA/ProblemRNA.java
@@ -1,22 +1,11 @@
 
-/**
- * @author (your name)
- * @version (a version number or a date)
- */
-public class ProblemRNA
+public class R2
 {
-    public static void main() {
-        // replace this string with the one from Project Rosalind
-        final String inputDNA = "GATGGAACTTGACTACGTAAATT";
-        final String output = transcribeDNAtoRNA(inputDNA);
-        // you can copy and paste from the terminal window into 
-        //   Project Rosalind to check your answer
-        System.out.println(output);
+    public static void main(String[] args){
+    String dna = "GATGGAACTTGACTACGTAAATT";
+    
+    String rna = dna.replace('T','U');
+       
+    System.out.println(rna); 
     }
-        
-    // This should take the given string and return a string
-    //   that replaces 'T' with 'U'
-    private static String transcribeDNAtoRNA(String dnaString) {
-        return "GAUGGAACUUGACUACGUAAAUU";
-    }    
 }
