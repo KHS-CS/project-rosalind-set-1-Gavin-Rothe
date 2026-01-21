b919f68be1e5aaa692e04178a6362fa79c0a9280
Gavin-Rothe <gavin.rothe@kentshill.org>
2026-01-21 09:12:51 -0500

    Update ProblemDNA.java

--- DNA/ProblemDNA.java
+++ DNA/ProblemDNA.java
@@ -1,37 +1,19 @@
-
-/**
- * Write a description of class ProblemDNA here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class ProblemDNA
+public class DNA_prob1
 {
-    public static void main() {
-        // replace this string with the one from Project Rosalind
-        final String data = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
-        final int[] exampleExpectedCounts = new int[]{20,12,17,21};
-        int[] output = nucleotideCounts(data);
-        // you can copy and paste from the terminal window into 
-        //   Project Rosalind to check your answer
-        System.out.println(formatData(output));
-    }
-    
-    // This should take the array of counts and return a 
-    //   space-delimited String with the respective counts
-    //   in A C G T order
-    private static String formatData(int[] counts) {
-        String output = "";
-        for( int count : counts ) {
-            output += count + " ";
+    public static void main(String[] args){
+        
+        String dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+        int A = 0, C = 0, G = 0, T = 0;
+
+        for (int i = 0; i < dna.length(); i++) {
+            char c = dna.charAt(i);
+
+            if (c == 'A') A++;
+            if (c == 'C') C++;
+            if (c == 'G') G++;
+            if (c == 'T') T++;
         }
-        return output; 
+
+        System.out.println(A + " " + C + " " + G + " " + T);
     }
-    
-    // This should take the given string and return an int
-    //   array of how many times each letter occurs in the 
-    //   string. Ideally in A C G T order. 
-    private static int[] nucleotideCounts(String dnaString) {
-        return new int[]{20,12,17,21};
     }
-}
