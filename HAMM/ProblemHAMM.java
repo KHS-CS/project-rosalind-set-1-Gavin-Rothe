6ade32e1d2c6076f0b949beead39f5232503bb78
Gavin-Rothe <gavin.rothe@kentshill.org>
2026-01-23 09:07:57 -0500

    Update ProblemHAMM.java

--- HAMM/ProblemHAMM.java
+++ HAMM/ProblemHAMM.java
@@ -1,22 +1,15 @@
 
-/**
- * @author (your name)
- * @version (a version number or a date)
- */
-public class ProblemHAMM {
-    public static void main() {
-        // replace this string with the one from Project Rosalind
+public class R4 {
+    public static void main(String[] args) {
+    
         final String inputDNA1 = "GAGCCTACTAACGGGAT";
         final String inputDNA2 = "CATCGTAATGACGGCCT";
-        final int output = hammingDistance(inputDNA1, inputDNA2);
-        // you can copy and paste from the terminal window into 
-        //   Project Rosalind to check your answer
-        System.out.println(output);
+        int e = 0;
+        for(int i = 0; i < inputDNA1.length(); i++){
+            char c = inputDNA1.charAt(i);
+            char d = inputDNA2.charAt(i);
+            if(c != d) e++;
+        }
+        System.out.println(e);
     }
-        
-    // This should take the given strings and return the number
-    //   of times they differ at the same location
-    private static int hammingDistance(String a, String b) {
-        return 7;
-    }    
 }
